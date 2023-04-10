14819074ae82d7e0173dd9c25427713fe94e7df9
Sérgio Lima <sergiolimafg3@gmail.com>
2023-04-10 09:46:20 -0300

    isserindo input controlado

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,13 +5,10 @@ const Time = ({ time, colaboradores, aoDeletar }) => {
 
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input value={Time.corSecundario} type='color' className='input-cor' /> 
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                    // console.log(<Colaborador key={indice} 
-                    //     colaborador={colaborador} 
-                    //     corDeFundo={time.corSecundaria} 
-                    //     aoDeletar={aoDeletar}/>)
                     return <Colaborador key={indice} 
                     colaborador={colaborador} 
                     corDeFundo={time.corSecundaria} 
